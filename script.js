d8bb4ef4b7a907d9b25243792ec04e0767a3333f
Brandon Lentz <brandonlentz1214@gmail.com>
2025-01-16 21:10:53 -0500

    added javascript

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#FF6B6B', '#4ECDC4', '#45B7D1', '#96CEB4', '#FFEEAD'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.querySelector('h1').style.color = randomColor;
-});
